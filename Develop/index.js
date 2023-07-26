7364c551f2f68e0e447128f61f6768187c2d277a
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-07-26 13:31:17 -0500

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,59 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = require('./utils/questions');
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What command should be run for testing?",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE",
+            "ART",
+            "None"
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
